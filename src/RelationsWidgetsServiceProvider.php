19f7da93104b75370f0cc459e1b8b27fdec4db63
Kevin Pittevils <kevin.pittevils@androme.be>
2023-12-01 15:05:39 +0100

    - Add automatic service provider

--- src/RelationsWidgetsServiceProvider.php
+++ src/RelationsWidgetsServiceProvider.php
@@ -6,13 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class RelationsWidgetsServiceProvider extends ServiceProvider
 {
-    /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->loadViewsFrom(realpath(__DIR__.'/resources/views'), 'backpack');
-    }
+    use AutomaticServiceProvider;
+
+    protected $vendorName = 'izica';
+    protected $packageName = 'relations-widgets-for-backpack';
+    protected $commands = [];
+
 }
